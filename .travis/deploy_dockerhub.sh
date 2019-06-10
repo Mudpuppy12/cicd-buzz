1fe535aacc53b86396d03d1d1b81a5d2965a7a04
vagrant <vagrant@sandbox.internal.lan>
2019-06-10 16:58:05 +0000

    Added echo

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+echo "Running Docker build" 
+echo "STEP 1: Login"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
+
+echo "Step 2: Build"
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+echo "Step 3: Push"
 docker push $TRAVIS_REPO_SLUG:$TAG
